69cc453f338bf10f2b4bf0f55bcc4041f2a910b4
omordkovich <o.mordkovich@hotmail.com>
2026-08-25 15:22:07 +0200

    added side gfx

--- src/app/milans7BD/page.tsx
+++ src/app/milans7BD/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { Footer } from "@/components/Footer";
 import { GuestSection } from "@/components/GuestSection";
 import { Header } from "@/components/Header";
@@ -5,13 +6,30 @@ import { Hero } from "@/components/Hero";
 
 export default function PartyPage() {
   return (
-    <div className="flex min-h-full flex-col">
+    <div className="relative flex min-h-full flex-col">
       <Header />
       <main className="flex-1">
         <Hero />
         <GuestSection />
       </main>
       <Footer />
+
+      <Image
+        src="/plants-left.png"
+        alt=""
+        width={434}
+        height={1024}
+        aria-hidden="true"
+        className="pointer-events-none absolute bottom-0 left-0 z-0 hidden h-[320px] w-auto object-contain object-bottom lg:block xl:h-[420px] 2xl:h-[520px]"
+      />
+      <Image
+        src="/plants-right.png"
+        alt=""
+        width={283}
+        height={1024}
+        aria-hidden="true"
+        className="pointer-events-none absolute right-0 bottom-0 z-0 hidden h-[320px] w-auto object-contain object-bottom lg:block xl:h-[420px] 2xl:h-[520px]"
+      />
     </div>
   );
 }
